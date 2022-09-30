f557850eb58584aafa5a16e27a6c0deadf73e21f
onebit0fme <yanchynskyy@pm.me>
2022-09-30 14:49:29 -0400

    Extra boots; Form refactoring

--- src/components/DonateForm.tsx
+++ src/components/DonateForm.tsx
@@ -9,7 +9,9 @@ export interface DonateFormProps {
 
 const REGISTRATION_PRODUCT_ID = "QnVzaW5lc3M6YWU4YTgxYjYtZWI4OS00MDRhLWExNzgtYzJmYmM4OTc2ODIzO1Byb2R1Y3Q6ODIxNjg1MDQ="
 const SOCKS_PRODUCT_ID = "QnVzaW5lc3M6YWU4YTgxYjYtZWI4OS00MDRhLWExNzgtYzJmYmM4OTc2ODIzO1Byb2R1Y3Q6ODIxNjg1NDM="
-const DELIVERY_PRODUCT_ID = "QnVzaW5lc3M6YWU4YTgxYjYtZWI4OS00MDRhLWExNzgtYzJmYmM4OTc2ODIzO1Byb2R1Y3Q6ODIyNzI1Mjg="
+const BOOTS_PRODUCT_ID = "QnVzaW5lc3M6YWU4YTgxYjYtZWI4OS00MDRhLWExNzgtYzJmYmM4OTc2ODIzO1Byb2R1Y3Q6ODIzNTU1MjI="
+const DELIVERY_CANADA_PRODUCT_ID = "QnVzaW5lc3M6YWU4YTgxYjYtZWI4OS00MDRhLWExNzgtYzJmYmM4OTc2ODIzO1Byb2R1Y3Q6ODIyNzI1Mjg="
+const DELIVERY_OUTSIDE_CANADA_ID = "QnVzaW5lc3M6YWU4YTgxYjYtZWI4OS00MDRhLWExNzgtYzJmYmM4OTc2ODIzO1Byb2R1Y3Q6ODIzNTU4Mjc="
 
 const provinceOptions = [
     {"label": "Alberta (AB)", "value": "CA-AB"},
@@ -41,11 +43,10 @@ function DonateForm(props: DonateFormProps) {
     const [memo, setMemo] = useState("");
     const [addNote, setAddNote] = useState(false);
     const [itemsSocks, setItemsSocks] = useState(1);
+    const [itemsExtraBoots, setItemsExtraBoots] = useState(0);
     const [loading, setLoading] = useState(false);
 
-    // const provinceCode = addressProvince ? provinceCodes[provinces.indexOf(addressProvince)] : undefined;
-
-    const total = 60 + itemsSocks * 20 + 10;
+    const total = 60 + itemsExtraBoots * 60 + itemsSocks * 20 + 10;
 
     const step1done = !!email && !!fullName;
     const step2done = step1done && !!addressLine1 && !!addressCity && !!addressCountry && !!addressProvince && !!addressCountry && !!phoneNumber;
@@ -74,11 +75,15 @@ function DonateForm(props: DonateFormProps) {
                         'quantity': 1,
                         'unitPrice': 6000,
                     },
+                    [BOOTS_PRODUCT_ID]: {
+                        'quantity': itemsExtraBoots,
+                        'unitPrice': 6000,
+                    },
                     [SOCKS_PRODUCT_ID]: {
                         'quantity': itemsSocks,
                         'unitPrice': 2000,
                     },
-                    [DELIVERY_PRODUCT_ID]: {
+                    [DELIVERY_CANADA_PRODUCT_ID]: {
                         'quantity': 1,
                         'unitPrice': 1000,
                     },
@@ -116,7 +121,7 @@ function DonateForm(props: DonateFormProps) {
                                 type="text"
                                 name="fullName"
                                 id="register-name"
-                                className="form-control"
+                                className="form-control form-control-lg"
                                 value={fullName}
                                 placeholder="Напишіть Ваше ім'я"
                                 onChange={(e) => setFullname(e.target.value)}
@@ -128,49 +133,28 @@ function DonateForm(props: DonateFormProps) {
                                 type="text"
                                 name="email"
                                 id="register-email"
-                                className="form-control"
+                                className="form-control form-control-lg"
                                 value={email}
                                 placeholder="you@yoursite.com"
                                 onChange={(e) => setEmail(e.target.value)}
                             />
                         </div>
-                        <button type="button" className="btn btn-primary-2 sw-btn-next" onClick={() => setWizardStep(1)} disabled={!email || !fullName}>Далі</button>
+                        {step1done ? <button type="button" className="btn btn-primary-2 sw-btn-next" onClick={() => setWizardStep(1)} disabled={!step1done}>Далі</button> : null}
                     </div>
                 </div>
                 <div id="step-2" className={wizardStep == 1 ? "active tab-pane step-content" : "tab-pane step-content"}>
                     <div className="p-4">
                         <div className="form-group">
-                            <label htmlFor="register-email">Поштові Деталі</label>
+                            <label htmlFor="register-address-country">Поштові Деталі</label>
                             <input
                                 type="text"
-                                name="address-line-1"
-                                id="register-address-line-1"
+                                name="address-country"
+                                id="register-address-country"
                                 className="form-control"
-                                value={addressLine1}
-                                placeholder="Номер будинку і вулиця"
-                                onChange={(e) => setAddressLine1(e.target.value)}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <input
-                                type="text"
-                                name="address-line-2"
-                                id="register-address-line-2"
-                                className="form-control"
-                                value={addressLine2}
-                                placeholder="Номер квартири (apt, suite, unit)"
-                                onChange={(e) => setAddressLine2(e.target.value)}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <input
-                                type="text"
-                                name="address-city"
-                                id="register-address-city"
-                                className="form-control"
-                                value={addressCity}
-                                placeholder="Місто"
-                                onChange={(e) => setAddressCity(e.target.value)}
+                                value={'Canada'}
+                                placeholder="Країна"
+                                disabled
+                                onChange={(e) => setAddressCountry(e.target.value)}
                             />
                         </div>
                         {/* <div className="form-group">
@@ -200,13 +184,34 @@ function DonateForm(props: DonateFormProps) {
                         <div className="form-group">
                             <input
                                 type="text"
-                                name="address-country"
-                                id="register-address-country"
+                                name="address-city"
+                                id="register-address-city"
                                 className="form-control"
-                                value={'Canada'}
-                                placeholder="Країна"
-                                disabled
-                                onChange={(e) => setAddressCountry(e.target.value)}
+                                value={addressCity}
+                                placeholder="Місто"
+                                onChange={(e) => setAddressCity(e.target.value)}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                name="address-line-1"
+                                id="register-address-line-1"
+                                className="form-control"
+                                value={addressLine1}
+                                placeholder="Номер будинку і вулиця"
+                                onChange={(e) => setAddressLine1(e.target.value)}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                name="address-line-2"
+                                id="register-address-line-2"
+                                className="form-control"
+                                value={addressLine2}
+                                placeholder="Номер квартири (apt, suite, unit)"
+                                onChange={(e) => setAddressLine2(e.target.value)}
                             />
                         </div>
                         <div className="form-group">
@@ -231,7 +236,7 @@ function DonateForm(props: DonateFormProps) {
                                 onChange={(e) => setPhoneNumber(e.target.value)}
                             />
                         </div>
-                        <button type="button" className="btn btn-primary-2 sw-btn-next" onClick={() => setWizardStep(2)} disabled={!addressLine1 || !addressCity || !addressCountry || !addressProvince || !phoneNumber}>Далі</button>
+                        {step2done ? <button type="button" className="btn btn-primary-2 sw-btn-next" onClick={() => setWizardStep(2)} disabled={!step2done}>Далі</button> : null }
                     </div>
                 </div>
                 <div id="step-3" className={wizardStep == 2 ? "active tab-pane step-content" : "tab-pane step-content"}>
@@ -240,38 +245,61 @@ function DonateForm(props: DonateFormProps) {
                         <table className="table table-hover">
                             <tbody>
                                 <tr>
-                                    <td><strong>Стартовий внесок</strong> <br /> <small>Усі кошти від вашої реєстрації будуть спрямовані на закупівлю зимового взуття для українських захисників</small></td>
+                                    <td>
+                                        <strong>Стартовий внесок</strong> - одна пара взуття для ЗСУ <br /> <small>Усі кошти від вашої реєстрації будуть спрямовані на закупівлю зимового взуття для українських захисників</small>
+                                    </td>
                                     <td>$60</td>
                                 </tr>
-                                {itemsSocks > 0 ? (
-                                    <tr>
-                                        <td><strong>Військові шкарпетки {itemsSocks > 1 ? `(x${itemsSocks})` : ""}</strong></td>
-                                        <td>${itemsSocks * 20}</td>
-                                    </tr>
-                                ) : null}
+                                <tr>
+                                    <td>
+                                        <div>
+                                            <label htmlFor="register-socks">🥾 {itemsExtraBoots > 0 ? <><strong>Взуття</strong> для ЗСУ</> : <>Додати <strong>ще взуття</strong> для ЗСУ?</>} <br/><small>Одна пара військового взуття <a href="https://www.patriboots.com.ua/shop/otaman-khaki-nu/" target="_blank">PatriBoots Otaman</a> $60</small></label>
+                                            <input type="range" className="custom-range" id="register-socks" min={0} max={10} value={itemsExtraBoots} onChange={(e) => setItemsExtraBoots(parseInt(e.target.value))} />
+                                        </div>
+                                        <div className="custom-range-labels">
+                                            <span>0</span>
+                                            <span>1</span>
+                                            <span>2</span>
+                                            <span>3</span>
+                                            <span>4</span>
+                                            <span>5</span>
+                                            <span>6</span>
+                                            <span>7</span>
+                                            <span>8</span>
+                                            <span>9</span>
+                                            <span>10</span>
+                                        </div>
+                                    </td>
+                                    <td>{itemsExtraBoots > 0 ? `$${itemsExtraBoots * 60}` : ""}</td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <p>🧦 {itemsSocks > 0 ? <><strong>Військові шкарпетки</strong> для ЗСУ</> : <>Додати <strong>військові шкарпетки</strong> для ЗСУ?</>} <br/><small>Виробник професійних військових шкарпеток <a href="https://covertthreads.com" target="_blank">Covert Threads (USA)</a> підтримує Україну і надає нам суттєву знижку (по собівартості). Одна пара якісних шкарпеток з доставкою $20</small></p>
+                                        <div>
+                                            <input type="range" className="custom-range" id="register-socks" min={0} max={10} value={itemsSocks} onChange={(e) => setItemsSocks(parseInt(e.target.value))} />
+                                        </div>
+                                        <div className="custom-range-labels">
+                                            <span>0</span>
+                                            <span>1</span>
+                                            <span>2</span>
+                                            <span>3</span>
+                                            <span>4</span>
+                                            <span>5</span>
+                                            <span>6</span>
+                                            <span>7</span>
+                                            <span>8</span>
+                                            <span>9</span>
+                                            <span>10</span>
+                                        </div>
+                                    </td>
+                                    <td>{itemsSocks > 0 ? `$${itemsSocks * 20}` : ""}</td>
+                                </tr>
                                 <tr>
                                     <td><strong>Доставка</strong> <br /> <small>(Канада)</small></td>
                                     <td>$10</td>
                                 </tr>
                             </tbody>
                         </table>
-                        <div>
-                            <label htmlFor="register-socks">🧦 Додати військові шкарпетки? Виробник професійних військових шкарпеток <a href="https://covertthreads.com" target="_blank">Covert Threads (USA)</a> підтримує Україну і надає нам суттєву знижку (по собівартості). Одна пара якісних шкарпеток з доставкою $20</label>
-                            <input type="range" className="custom-range" id="register-socks" min={0} max={10} value={itemsSocks} onChange={(e) => setItemsSocks(parseInt(e.target.value))} />
-                        </div>
-                        <div className="custom-range-labels">
-                            <span>0</span>
-                            <span>1</span>
-                            <span>2</span>
-                            <span>3</span>
-                            <span>4</span>
-                            <span>5</span>
-                            <span>6</span>
-                            <span>7</span>
-                            <span>8</span>
-                            <span>9</span>
-                            <span>10</span>
-                        </div>
                         {addNote ? (
                             <div className="form-group mt-2">
                                 <label htmlFor="register-memo">Коментар (<a href="#" onClick={handleAddNote}>та ні</a>)</label>
